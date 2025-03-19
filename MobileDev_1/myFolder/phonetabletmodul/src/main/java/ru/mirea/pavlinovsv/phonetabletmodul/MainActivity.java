c1632a48cdbc0673808c4f5020b376b9ea6dad67
Sergey <pavlinov020304@mail.ru>
2025-03-19 16:03:09 +0300

    MobileDev_1

---
+++ MobileDev_1/myFolder/phonetabletmodul/src/main/java/ru/mirea/pavlinovsv/phonetabletmodul/MainActivity.java
@@ -0,0 +1,52 @@
+package ru.mirea.pavlinovsv.phonetabletmodul;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private CheckBox checkBox;
+    private Button btnWhoAmI;
+    private Button btnItIsNotMe;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        checkBox = findViewById(R.id.checkBox);
+        btnWhoAmI = findViewById(R.id.btnWhoAmI);
+        btnItIsNotMe = findViewById(R.id.btnItIsNotMe);
+
+        View.OnClickListener oclBtnWhoAmI = new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                checkBox.setText("Мой номер по списку № 18");
+                checkBox.setChecked(true);
+            }
+        };
+        btnWhoAmI.setOnClickListener((oclBtnWhoAmI));
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void ClickMe(View view) {
+        checkBox.setText("Это не я сделал");
+        checkBox.setChecked(false);
+    }
+}
